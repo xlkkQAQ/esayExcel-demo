3a501f9e2a6424402dc2b96b72082582b5f19856
xlkk <60306671+xlkkqaq@users.noreply.github.com>
2022-08-02 11:03:57 +0800

    Initial commit

---
+++ src/main/java/com/xlkk/EsayExcelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xlkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsayExcelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsayExcelDemoApplication.class, args);
+	}
+
+}
